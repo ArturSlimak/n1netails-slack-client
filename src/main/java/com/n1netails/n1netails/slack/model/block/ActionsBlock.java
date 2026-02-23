e8eea5ade97f369eec350e60898eca450dac41f8
ArturSlimac <artur.nenakhov@student.hogent.be>
2026-02-23 12:28:32 +0100

    new block

---
+++ src/main/java/com/n1netails/n1netails/slack/model/block/ActionsBlock.java
@@ -0,0 +1,30 @@
+package com.n1netails.n1netails.slack.model.block;
+
+import com.n1netails.n1netails.slack.model.SlackBlock;
+import com.n1netails.n1netails.slack.model.SlackElement;
+import com.slack.api.model.block.LayoutBlock;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ActionsBlock implements SlackBlock {
+
+    private final List<SlackElement> elements;
+
+    public ActionsBlock(List<SlackElement> elements) {
+        this.elements = elements;
+    }
+
+    @Override
+    public LayoutBlock toLayoutBlock() {
+        return com.slack.api.model.block.ActionsBlock.builder()
+                .elements(
+                        elements.stream()
+                                .map(SlackElement::toBlockElement)
+                                .toList()
+                )
+                .build();
+    }
+}
